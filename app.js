69deb35e7fba1620a3b346097ee76039a7eef48b
Michael Petrov <mpetrovcello@gmail.com>
2020-11-07 16:00:00 +0000

    made some more changes

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 const cello = document.getElementById(`cello`)
+const h1 = document.querySelector(`h1`)
+
+const lis = document.querySelectorAll(`li`)
+const colors = [`red`, `orange`, `purple`]
+lis.forEach((li, i) => {
+  const color = colors[i];
+  li.style.color = color;
+})
